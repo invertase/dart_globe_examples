f4c965103dfd0d673c80ed662dd3562003c35cb3
Mike Diarmid <mike.diarmid@gmail.com>
2023-06-21 18:36:40 +0100

    chore: another preview

--- examples/dart_frog/routes/index.dart
+++ examples/dart_frog/routes/index.dart
@@ -1,5 +1,6 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
+  print('We have logs now');
   return Response(body: 'Welcome to Dart Frog running in Globe! And then lets deploy a preview of this change!');
 }
