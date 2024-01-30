675e7ba62361c1461addc8dc3826dd109d544929
Majid <mhadaily@gmail.com>
2024-01-30 11:06:20 +0100

    add dart fake api example

---
+++ examples/dart_fake_api/frontend/flutter_client/lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_client/model.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final uri = Uri.https('mockdash.globeapp.dev', '/api/v1/users');
+    return Scaffold(
+      appBar: AppBar(title: const Text('Testing mockdash.globeapp.dev')),
+      body: FutureBuilder(
+        future: http.get(uri),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong!'));
+          }
+          if (snapshot.hasData && snapshot.data != null) {
+            final users = Users.fromJson(jsonDecode(snapshot.data!.body));
+            return ListView.separated(
+              itemCount: users.data.length,
+              itemBuilder: (context, index) {
+                final user = users.data[index];
+                return ListTile(
+                  title: Text('${user.firstName} ${user.lastName}'),
+                  subtitle: Text(user.job),
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(),
+            );
+          }
+          return const Center(child: Text('No Users Yet!'));
+        },
+      ),
+    );
+  }
+}
---
+++ examples/dart_fake_api/frontend/flutter_client/lib/model.dart
@@ -0,0 +1,72 @@
+class Users {
+  final List<User> data;
+  final int total;
+  final int limit;
+  final int skip;
+
+  Users({
+    required this.data,
+    required this.total,
+    required this.limit,
+    required this.skip,
+  });
+
+  factory Users.fromJson(Map<String, dynamic> json) {
+    return Users(
+      data: json['data'],
+      total: json['total'],
+      limit: json['limit'],
+      skip: json['skip'],
+    );
+  }
+}
+
+class User {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String address;
+  final String bio;
+  final String phone;
+  final String image;
+  final String company;
+  final String job;
+  final String website;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.address,
+    required this.bio,
+    required this.phone,
+    required this.image,
+    required this.company,
+    required this.job,
+    required this.website,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      email: json['email'],
+      address: json['address'],
+      bio: json['bio'],
+      phone: json['phone'],
+      image: json['image'],
+      company: json['company'],
+      job: json['job'],
+      website: json['website'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+    );
+  }
+}
