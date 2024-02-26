abe60e0436ec83ddbeb53657f88845917acf5ea4
Majid <mhadaily@gmail.com>
2024-02-26 08:54:16 +0100

    test token

--- examples/utility_backend/routes/index.dart
+++ examples/utility_backend/routes/index.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 import '../helpers/logger.dart';
 
 Response onRequest(RequestContext context) {
   logger.d('We have logs now');
+  final testToken = Platform.environment['API_TOKEN'];
   return Response(
-    body: 'Welcome to Dart Frog running in Globe!',
+    body: 'Welcome to Dart Frog running in Globe! $testToken',
   );
 }
