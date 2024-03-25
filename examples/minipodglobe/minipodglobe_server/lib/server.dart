8a0f52e8a21b23301bd0542f02a9b1a0facced91
Majid <mhadaily@gmail.com>
2024-03-25 09:30:29 +0100

    prepare for globe

--- examples/minipodglobe/minipodglobe_server/lib/server.dart
+++ examples/minipodglobe/minipodglobe_server/lib/server.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:serverpod/serverpod.dart';
 
 import 'src/generated/protocol.dart';
@@ -6,8 +8,16 @@ import 'src/generated/endpoints.dart';
 // This is the starting point of your Serverpod server.
 void run(List<String> args) async {
   // Initialize Serverpod and connect it with your generated code.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final config = ServerConfig(
+    port: port,
+    publicScheme: 'http',
+    publicHost: 'localhost',
+    publicPort: port,
+  );
   final pod = Serverpod(
     args,
+    config: ServerpodConfig(apiServer: config),
     Protocol(),
     Endpoints(),
   );
